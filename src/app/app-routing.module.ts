0d7a36f736bf0599fa733a93ce2939b7387beffd
niqart <nlaraya@uc.cl>
2021-04-26 11:30:29 -0400

    ✨ routing and basic modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ShipInfoComponent } from './pages/shop/ship-info/ship-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'product/:id', component: ProductComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'shop/info', component: ShipInfoComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/shop/ship-info/ship-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ship-info',
+  templateUrl: './ship-info.component.html',
+  styleUrls: ['./ship-info.component.scss']
+})
+export class ShipInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
