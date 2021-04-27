8b3f81e867a03147311ec0ddcf7b3583a2753ab8
niqart <nlaraya@uc.cl>
2021-04-27 09:49:26 -0400

    ✨ form validation

--- src/app/pages/shop/ship-info/ship-info.component.ts
+++ src/app/pages/shop/ship-info/ship-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ship-info',
@@ -17,26 +18,48 @@ export class ShipInfoComponent implements OnInit {
   });
 
   cardForm = new FormGroup({
-    type: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
-    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
-    number: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    secureCode: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    expDate: new FormControl('', [Validators.required, Validators.min(1), Validators.max(12)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+    number: new FormControl('', [Validators.required,
+    Validators.pattern('(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})')]),
+    secureCode: new FormControl('', [Validators.required, Validators.min(100), Validators.max(999)]),
+    expDate: new FormControl('', [Validators.required, Validators.pattern('(0[1-9]|1[0-2])\\/?([0-9]{4}|[0-9]{2})')]),
   });
 
   currentStep: number;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.currentStep = 0;
   }
 
 
-  get address(): AbstractControl { return this.shipForm.get('address'); }
+  get shipAddress(): AbstractControl { return this.shipForm.get('address'); }
+  get shipPostal(): AbstractControl { return this.shipForm.get('postal'); }
+  get shipCity(): AbstractControl { return this.shipForm.get('city'); }
+  get shipRegion(): AbstractControl { return this.shipForm.get('region'); }
+  get shipCountry(): AbstractControl { return this.shipForm.get('country'); }
+
+  get cardName(): AbstractControl { return this.cardForm.get('name'); }
+  get cardNumber(): AbstractControl { return this.cardForm.get('number'); }
+  get cardCode(): AbstractControl { return this.cardForm.get('secureCode'); }
+  get cardExpDate(): AbstractControl { return this.cardForm.get('expDate'); }
 
   checkShipForm(): void {
-    this.currentStep++;
+    if (this.shipForm.valid) { this.currentStep = 1; }
+    else { alert('No se han ingresado todos los datos de envío'); }
+  }
+
+  checkCardForm(): void {
+    if (this.cardForm.valid) {
+      this.router.navigate(['/home']);
+    }
+    else { alert('No se han ingresado todos los datos de pago'); }
   }
 
 }
+
+
+/* Credit Card Validation from https://stackoverflow.com/questions/9315647/regex-credit-card-number-tests
+    https://stackoverflow.com/questions/20430391/regular-expression-to-match-credit-card-expiration-date
+*/
