f866f1a189d63b5b7145eee62d4c6766f2c5d49e
niqart <nlaraya@uc.cl>
2021-04-26 23:20:51 -0400

    🚧 shop forms

--- src/app/pages/shop/ship-info/ship-info.component.ts
+++ src/app/pages/shop/ship-info/ship-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ship-info',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShipInfoComponent implements OnInit {
 
+  shipForm = new FormGroup({
+    country: new FormControl('', [Validators.required]),
+    region: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    postal: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
+
+  cardForm = new FormGroup({
+    type: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+    number: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    secureCode: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    expDate: new FormControl('', [Validators.required, Validators.min(1), Validators.max(12)]),
+  });
+
+  currentStep: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.currentStep = 0;
+  }
+
+
+  get address(): AbstractControl { return this.shipForm.get('address'); }
+
+  checkShipForm(): void {
+    this.currentStep++;
   }
 
 }
